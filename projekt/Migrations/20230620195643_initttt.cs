e6f27309041ff118b17d54301b550185d83a6a88
Karol Wierzbiński <167864@o365.prz.edu.pl>
2023-06-20 21:57:21 +0200

    last

---
+++ projekt/Migrations/20230620195643_initttt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projekt.Migrations
+{
+    public partial class initttt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
